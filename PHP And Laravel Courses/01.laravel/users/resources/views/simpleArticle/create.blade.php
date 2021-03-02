6c55461edf3f6f23eb75cfa8107dc73da64bf284
Ibn-mohey <abdelrhman.mohey96@gmail.com>
2021-03-02 04:04:34 +0200

    Update create.blade.php
    make the form looks better

--- PHP And Laravel Courses/01.laravel/users/resources/views/simpleArticle/create.blade.php
+++ PHP And Laravel Courses/01.laravel/users/resources/views/simpleArticle/create.blade.php
@@ -1,56 +1,50 @@
-
-
 @extends('simpleLayout')
 
 @section('head')
-<link href="/css/bulma.css" rel="stylesheet"/>
+    <link href="/css/bulma.css" rel="stylesheet" />
 @endsection
 
 @section('content')
 
-<div id="wrapper">
+    <div id="wrapper">
 
-    <div id="page" class="container">
-        <h1>
-            new Atricle
-        </h1>
+        <div id="page" class="container">
+            <h1>
+                new Atricle
+            </h1>
+            <form action="">
+                <div class="field">
+                    <label class="label" for="title">Title</label>
+
+                    <div class="control">
+                        <input class="input" type="text"  name="title" id="title">
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label" for="excerpt">Excerpt</label>
+
+                    <div class="control">
+                        <textarea class="textarea" name="excerpt" id="excerpt"></textarea>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label" for="body">Body</label>
+
+                    <div class="control">
+                        <textarea class="textarea" name="body" id="body"></textarea>
+                    </div>
+                </div>
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-link" type="submit">Submit</button>
+                    </div>
+                </div>
+            </form>
+
+
     </div>
 
-<form method = "POST" action="/simpleArticles">
-    @csrf
-    <div class="field">
-        <label class="label" for = "title">Name</label>
-        <div class="control">
-          <input class="input" type="text" name="title" id="title">
-        </div>
-      </div>
-
-      <div class="field">
-        <label class="label" for = "excerpt">excerpt</label>
-        <div class="control">
-          <textarea class="texrarea"  name="excerpt" id="excerpt"> </textarea>
-
-        </div>
-      </div>
-
-      <div class="field">
-        <label class="label" for = "body">body</label>
-        <div class="control">
-          <textarea class="texrarea"  name="body" id="body"> </textarea>
-        </div>
-      </div>
-
-      <div class="field is-grouped">
-        <div class="control">
-          <button class="button is-link">Submit</button>
-        </div>
-
-
-
 </div>
-
-
-</form>
-
-
-@endsection
+    @endsection
